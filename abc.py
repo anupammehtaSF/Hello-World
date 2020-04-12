e22219076de61bf1599727b4d7d55bd98a94ad00
Anupam Mehta <60706925+anupammehtaSF@users.noreply.github.com>
2020-04-12 15:31:42 -0400

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,1 +1,8 @@
-print ("Here")
+from subprocess import PIPE,Popen
+
+process = Popen(['ls','---al'], stdout=PIPE,shell=True)
+
+(output, err) = process.communicate()
+exit_code = process.wait()
+
+print (output)
