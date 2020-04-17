6a22b018abba17e29c047e648e498a5e06dcb441
Anupam Mehta <60706925+anupammehtaSF@users.noreply.github.com>
2020-04-17 10:31:08 -0400

    Update shell-script.sh

--- shell-script.sh
+++ shell-script.sh
@@ -2,7 +2,7 @@
 cat /proc/partitions
 # identify the disk to mount - find the major and minor numbers
 mknod blk b 253 1
-mkdir rootfs
-mount blk rootfs
-cd rootfs
+mkdir rootfs2
+mount blk rootfs2
+cd rootfs2
 ls -la
